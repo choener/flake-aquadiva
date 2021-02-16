86a0d964de98c6da91986a1a510fb7a881b9eac9
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2021-02-16 17:50:00 +0100

    provide dev env for packages

--- flake.nix
+++ flake.nix
@@ -1,11 +1,36 @@
 {
   description = "Repository of software to support AquaDiva";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }: let
+
+    # each system
+    eachSystem = system: let
+
+      pkgs = import nixpkgs { inherit system; };
+      # fhs with things we need
+      fhs = pkgs.buildFHSUserEnv {
+        name = "fhs";
+        targetPkgs = p: [ p.jdk14 ];
+      };
+
+    in rec {
+      devShell = pkgs.stdenv.mkDerivation {
+        name = "AquaDiva";
+        nativeBuildInputs = [ fhs ];
+        shellHook = ''
+          ${fhs}/bin/AquaDivaFHS
+        '';
+      }; # devShell
+      apps.fhs = { type = "app"; program = "${fhs}/bin/fhs"; };
+      # by default, we get the @fhs@ environment to play around in.
+      defaultApp = apps.fhs;
+    }; # eachSystem
+
+  in
+    flake-utils.lib.eachDefaultSystem eachSystem;
 }
